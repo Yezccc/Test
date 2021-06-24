003f2902ee721ace29aa2cc24644d05eb6fd7635
Yez <yez.ccc@gmail.com>
2021-06-24 19:00:58 +0800

    主函数备注

--- Pz/main.cpp
+++ Pz/main.cpp
@@ -6,13 +6,13 @@
 //  
 
 
-/*
- #include <iostream>
+//主函数
+#include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!!!\n";
     return 0;
 }
-*/
+
 
