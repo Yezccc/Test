fcc9a179a66519a6f8148e5469a585420f294f5d
Yez <yez.ccc@gmail.com>
2021-06-24 18:41:51 +0800

    new

---
+++ Pz/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Pz
+//
+//  Created by Yez on 2021/6/19.
+//  
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!!!\n";
+    return 0;
+}
+
